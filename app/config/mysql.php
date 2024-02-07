873832ede70c9f4311c636015ac7e7621ecbc1cf
Xelaj <ajuillard69@gmail.com>
2024-02-07 17:53:10 +0100

    first commit

---
+++ app/config/mysql.php
@@ -0,0 +1,16 @@
+<?php
+
+try {
+    $db = new PDO(
+     "mysql:host=dataBase;dbname=data_site;charset=utf8mb4",
+      'root',
+      null,
+      [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+);
+}
+catch(PDOException $error) {
+    echo $error ->getMessage();
+    die();
+}
+
+//fichier qui connecte à la base de données
